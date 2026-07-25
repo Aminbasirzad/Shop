f016b2d92cfff04af9e4a67ae7d293b9dc0d3325
Your Name <you@example.com>
2026-07-25 06:40:05 -0700

    feat: implement views for rendering html templates

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+def page_not_found(request):
+  return render(request, 'pages/404.html')
+
+def about(request):
+  return render(request, 'pages/about-us.html')
+
+def team(request):
+  return render('pages/team.html')
+
+def faq(request):
+  return render(request, 'pages/faq.html')
+
+def privacy_policy(request):
+  return render(request, 'pages/privacy-policy.html')
+
+def terms_conditions(request):
+  return render(request, 'pages/terms-conditions.html')
+
+def contact(request):
+  return render(request, 'pages/contact-us.html')
+
+def testimonials(request):
+  return render(request, 'pages/testimonials.html')
