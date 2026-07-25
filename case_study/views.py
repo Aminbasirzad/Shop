67c13238a719dd5be91ca80341347c068cf40cfd
Your Name <you@example.com>
2026-07-25 11:55:50 -0700

    feat: add new app

--- case_study/views.py
+++ case_study/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def case_study(request):
-  return render(request, 'case_study/case_study.html')
+  return render(request, 'case_study/case-study.html')
 
 def case_study_detail(request):
-  return render(request, 'case_study/case_study_detail.html')
+  return render(request, 'case_study/cases-study-details.html')
