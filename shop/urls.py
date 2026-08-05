71b4d2a055978e15937b7b0697ef24fee1086d62
Your Name <you@example.com>
2026-08-05 05:58:54 -0700

    Fix: update navbar links and correct URL patterns in various templates

--- shop/urls.py
+++ shop/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 urlpatterns = [
   path('product_detail/<int:id>/', views.product_detail, name='product_detail'),
@@ -9,5 +8,3 @@ urlpatterns = [
   path('cart/', views.cart, name='cart'),
   path('checkout/', views.checkout, name='checkout'),
 ]
-if settings.DEBUG:
-  urlpatterns += static(settings.MEDIA_URL, docunent_root=settings.MEDIA_ROOT)
