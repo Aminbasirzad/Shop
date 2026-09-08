cf7411699f37fc459e47a13a0c61cff9eef06456
Your Name <you@example.com>
2026-09-08 09:44:25 -0700

    refactor: update core settings and home views

--- home/urls.py
+++ home/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('home/', views.home, name='home'),
+  path('', views.home, name='home'),
   path('home_v2/', views.home_v2, name='home_v2'),
   path('home_v3/', views.home_v3, name='home_v3'),
-  path('base/', views.base, name='base')
 ]
